bd69c55ad55dbc4c65e184e39adbf15aa4189ac0
Mike WInfield <lolwut-lol@yandex.ru>
2024-03-16 00:43:44 +0300

    Migration fix

--- db/migrations/public/002_actor.sql
+++ db/migrations/public/002_actor.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS public."actor"
 (
     id serial NOT NULL,
     name text NOT NULL,
+    gender text NOT NULL,
     dob date NOT NULL, 
     CONSTRAINT actor_pkey PRIMARY KEY (id)
 );
