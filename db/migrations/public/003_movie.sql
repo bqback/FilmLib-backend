3b7297c4d8206ceef825a14912b3b83d87f169c5
Mike WInfield <lolwut-lol@yandex.ru>
2024-03-15 23:15:45 +0300

    Migration fixes

--- db/migrations/public/003_movie.sql
+++ db/migrations/public/003_movie.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS public."movie"
     CONSTRAINT movie_pkey PRIMARY KEY (id),
     CONSTRAINT title_length_check CHECK (length(title) >= 1 AND length(title) <= 150),
     CONSTRAINT description_length_check CHECK (length(description) <= 150),
-    CONSTRAINT rating_check CHECK (rating >= 0 AND rating <= 10),
+    CONSTRAINT rating_check CHECK (rating >= 0 AND rating <= 10)
 );
 
 ---- create above / drop below ----
--- db/migrations/public/005_user.sql
+++ db/migrations/public/005_user.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS public."user"
 (
     id serial NOT NULL,
     login text NOT NULL,
-    password_hash string NOT NULL,
+    password_hash text NOT NULL,
     is_admin boolean DEFAULT false,
     CONSTRAINT user_pkey PRIMARY KEY (id)
 );
