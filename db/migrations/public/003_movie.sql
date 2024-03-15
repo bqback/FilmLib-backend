a5fa7866af68375ee911f53846e40915780f7aa4
Mike WInfield <lolwut-lol@yandex.ru>
2024-03-15 04:30:36 +0300

    DB setup migrations

---
+++ db/migrations/public/003_movie.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS public."movie"
+(
+    id serial NOT NULL,
+    title text NOT NULL,
+    description text NOT NULL,
+    release date NOT NULL, 
+    rating real NOT NULL,
+    CONSTRAINT movie_pkey PRIMARY KEY (id),
+    CONSTRAINT title_length_check CHECK (length(title) >= 1 AND length(title) <= 150),
+    CONSTRAINT description_length_check CHECK (length(description) <= 150),
+    CONSTRAINT rating_check CHECK (rating >= 0 AND rating <= 10),
+);
+
+---- create above / drop below ----
+
+DROP TABLE IF EXISTS public."movie";
