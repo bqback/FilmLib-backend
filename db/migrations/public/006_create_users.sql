ab7d21477d41664156de31728a3e4b523e0d8280
Mike WInfield <lolwut-lol@yandex.ru>
2024-03-15 20:18:47 +0300

    User migrations for auth

---
+++ db/migrations/public/006_create_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."user" VALUES 
+("user", "e172c5654dbc12d78ce1850a4f7956ba6e5a3d2ac40f0925fc6d691ebb54f6bf", false),
+("admin", "d82494f05d6917ba02f7aaa29689ccb444bb73f20380876cb05d1f37537b7892", true)
